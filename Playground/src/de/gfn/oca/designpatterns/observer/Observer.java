cc7be68d891cedb941f786eb46bddfcbd257cd80
Tomasz <tlubowiecki@10.7.10.28>
2018-05-07 14:32:26 +0200

    Observer

---
+++ Playground/src/de/gfn/oca/designpatterns/observer/Observer.java
@@ -0,0 +1,11 @@
+package de.gfn.oca.designpatterns.observer;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public interface Observer {
+    
+    void update(Status s);
+    
+}
---
+++ Playground/src/de/gfn/oca/designpatterns/observer/Status.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.oca.designpatterns.observer;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public enum Status {
+    
+    AN, AUS, PAUSE;
+    
+}
