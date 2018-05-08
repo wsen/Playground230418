8789f3ff609d6a7e356a4bd18804c503c0c10223
Tomasz <tlubowiecki@10.7.10.28>
2018-05-08 08:39:14 +0200

    Aufgabenblatt 3

---
+++ Playground/src/de/gfn/oca/aufgabenblatt3/aufgabe1/DreieckArten.java
@@ -0,0 +1,42 @@
+package de.gfn.oca.aufgabenblatt3.aufgabe1;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public abstract class DreieckArten implements FlaechenInhalt{
+    
+    private int a;
+    
+    private int b;
+    
+    private int c;
+
+    public DreieckArten(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getC() {
+        return c;
+    }
+    
+    public int flaeche() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int umfang() {
+        return a + b + c;
+    }
+    
+    public abstract int berechneWinkel();
+}
