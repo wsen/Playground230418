eb8cc9a23256e3dd6f1b91e0e1e45f55eafa3bfa
w_sen <w_sen@flashbox.de>
2018-05-15 23:55:58 +0200

    exp(home) Aufgaben

---
+++ Aufgaben/src/de/wsen/oca/teststuff/nr3/Beaver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wsen.oca.teststuff.nr3;
+
+/**
+ *
+ * @author wsen
+ */
+public class Beaver extends Rodent {
+
+    public Number chew() throws RuntimeException {
+        System.out.println("Beaver is chewing on wood");
+        return 2;
+    }
+}
---
+++ Aufgaben/src/de/wsen/oca/teststuff/nr3/Rodent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wsen.oca.teststuff.nr3;
+
+/**
+ *
+ * @author wsen
+ */
+public class Rodent {
+
+    public static Integer chew() throws Exception {
+        System.out.println("Rodent is chewing");
+        return 1;
+    }
+}
