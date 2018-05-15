4dc7c37838e3661c4b6385ee3c3ad262a989348c
wsen <w_sen@flashbox.de>
2018-05-15 16:42:44 +0200

    exmpl(gfn) polym boyarski questions

--- Playground/src/de/gfn/oca/polym/Primate.java
+++ Playground/src/de/gfn/oca/polym/Primate.java
@@ -10,5 +10,30 @@ package de.gfn.oca.polym;
  * @author student
  */
 public class Primate {
-    
+
+    public boolean hasHair() {
+        return true;
+    }
+}
+
+public interface HasTail {
+
+    public boolean isTailStriped();
+}
+
+public class Lemur extends Primate implements HasTail {
+
+    public boolean isTailStriped() {
+        return false;
+    }
+    public int age = 10;
+
+    public static void main(String[] args) {
+        Lemur lemur = new Lemur();
+        System.out.println(lemur.age); // Klassenvariable
+        HasTail hasTail = lemur;
+        System.out.println(hasTail.isTailStriped()); // Referenced interface method
+        Primate primate = lemur;
+        System.out.println(primate.hasHair()); //Referenced superClass method
+    }
 }
