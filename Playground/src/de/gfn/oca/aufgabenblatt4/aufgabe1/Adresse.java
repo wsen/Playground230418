d866a4e96b20c38fb058033c1e17efd4eb00df92
Tomasz <tlubowiecki@10.7.10.28>
2018-05-16 08:55:51 +0200

    Arbeitsblatt 4, Aufgabe 1

---
+++ Playground/src/de/gfn/oca/aufgabenblatt4/aufgabe1/Adresse.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.oca.aufgabenblatt4.aufgabe1;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public class Adresse {
+    
+    private static int anzahl = 0;
+    
+    private String vorname;
+    private String nachname;
+    private String strasse;
+    private String hausnummer;
+    private String plz;
+    private String ort;
+    private String land;
+
+    public static int getAnzahl() {
+        return anzahl;
+    }
+    
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+        anzahl++;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getHausnummer() {
+        return hausnummer;
+    }
+
+    public void setHausnummer(String hausnummer) {
+        this.hausnummer = hausnummer;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getLand() {
+        return land;
+    }
+
+    public void setLand(String land) {
+        this.land = land;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(vorname).append(" ").append(nachname)
+          .append("\n").append(strasse).append(" ").append(hausnummer)
+          .append("\n").append(plz).append(" ").append(ort)
+          .append("\n").append(land);
+        return sb.toString();
+    }
+}
---
+++ Playground/src/de/gfn/oca/aufgabenblatt4/aufgabe1/Verwaltung.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.oca.aufgabenblatt4.aufgabe1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public class Verwaltung {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        
+        outer:
+        while(!exit) {
+            
+            Adresse a = new Adresse();
+            System.out.print("\nVorname: ");
+            a.setVorname(scanner.next());
+            
+            System.out.print("Nachname: ");
+            a.setNachname(scanner.next());
+            
+            System.out.print("Strasse: ");
+            a.setStrasse(scanner.next());
+            
+            System.out.print("Hausnummer: ");
+            a.setHausnummer(scanner.next());
+            
+            System.out.print("PLZ: ");
+            a.setPlz(scanner.next());
+            
+            System.out.print("Ort: ");
+            a.setOrt(scanner.next());
+            
+            System.out.print("Land: ");
+            a.setLand(scanner.next());
+            
+            System.out.println("\nSie haben folgende Adresse eingegeben: ");
+            System.out.println(a);
+            
+            while(true) {
+                System.out.print("\nWollen Sie eine weitere Adresse eingeben (JA oder NEIN)? ");
+                String input = scanner.next().toLowerCase();
+                
+                if(input.equals("nein")) {
+                    System.out.println("\nSie haben " + a.getAnzahl() + " Datensätze eingegeben.");
+                    break outer;
+                }
+                else if(input.equals("ja")) {
+                    break;
+                }
+            }
+        }
+    }
+    
+}
