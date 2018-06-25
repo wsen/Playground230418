cab4c1cddc0e3682f9125ab4f75be5658303782d
w_sen <w_sen@flashbox.de>
2018-06-25 06:38:28 +0200

    le end of the weekend

---
+++ Playground/src/de/gfn/org/superprot/Superone.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.org.superprot;
+
+/**
+ *
+ * @author wsen
+ */
+public class Superone {
+    public static String superhans = "Hans";
+}
