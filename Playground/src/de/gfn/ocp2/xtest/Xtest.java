4c79e24ac34c688035e75de30edbb31e3f5aefd8
wsen <w_sen@flashbox.de>
2018-06-25 16:12:00 +0200

    boya(gfn) update ocp

--- Playground/src/de/gfn/ocp2/xtest/Xtest.java
+++ Playground/src/de/gfn/ocp2/xtest/Xtest.java
@@ -9,6 +9,54 @@ package de.gfn.ocp2.xtest;
  *
  * @author student
  */
-public class Xtest {
-    
+class Xtest {
+
+}
+
+public class Card {
+
+    private String rank;
+    private String suit;
+
+    public Card(String r, String s) {
+        if (r == null || s == null) {
+            throw new IllegalArgumentException();
+        }
+        rank = r;
+        suit = s;
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Card)) {
+            return false;
+        }
+        Card c = (Card) obj;
+        return rank.equals(c.rank) && suit.equals(c.suit);
+    }
+
+    public int hashCode() {
+        return rank.hashCode();
+    }
+}
+
+class Lion {
+
+    private int idNumber;
+    private int age;
+    private String name;
+
+    public Lion(int idNumber, int age, String name) {
+        this.idNumber = idNumber;
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Lion)) {
+            return false;
+        }
+        Lion otherLion = (Lion) obj;
+        return this.idNumber == otherLion.idNumber;
+    }
 }
