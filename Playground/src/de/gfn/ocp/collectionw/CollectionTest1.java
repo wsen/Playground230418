78eebce939087f1c85ed04dc7ac8e8ec22f0f715
w_sen <w_sen@flashbox.de>
2018-06-27 01:33:22 +0200

    chore(home) ArrayList studies

--- Playground/src/de/gfn/ocp/collectionw/CollectionTest1.java
+++ Playground/src/de/gfn/ocp/collectionw/CollectionTest1.java
@@ -12,7 +12,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
--- Playground/src/de/gfn/org/stringequals/StringEquals.java
+++ Playground/src/de/gfn/org/stringequals/StringEquals.java
@@ -5,6 +5,10 @@
  */
 package de.gfn.org.stringequals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author wsen
@@ -18,5 +22,26 @@ public class StringEquals {
         System.out.println(s1.equals(s2)+""); // true // true
         System.out.println(s1 == s2);         // true // false
         
+        System.out.println("---------------");
+        
+        String s = "Hello";
+        String t = new String(s);
+        if ("Hello".equals(s)) System.out.println("one - hello.equals(s)"); //true    one
+        if (t == s) System.out.println("two");              //false 
+        if (t.equals(s)) System.out.println("three - t.equals(s)");       //true  tree
+        if ("Hello" == s) System.out.println("four - ==");       //true  four
+        if ("Hello" == t) System.out.println("five");       //false
+        
+        System.out.println("-------------");
+        ArrayList l = new ArrayList();
+        l.size();
+        
+        System.out.println("-------------");
+        String [] names = {"Tom", "Dick", "Harry"};
+        //List<String> list = names.asList(); //!! There is no asList() method on an array instance
+        List<String> list = Arrays.asList(names);
+        list.set(0, "Sue");
+        System.out.println(names[0]);
+
     }
 }
