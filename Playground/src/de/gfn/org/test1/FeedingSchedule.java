8f924572b8da9f6bc9d93f44dd820a7f254a0307
w_sen <w_sen@flashbox.de>
2018-07-03 01:47:55 +0200

    chore(home) FeedingSchedule

--- Playground/src/de/gfn/org/test1/FeedingSchedule.java
+++ Playground/src/de/gfn/org/test1/FeedingSchedule.java
@@ -16,23 +16,23 @@ import java.util.List;
 public class FeedingSchedule {
 
     public static void main(String[] args) {
-//        boolean keepGoing = true;
-//        int count = 0;
-//        int x = 3;
-//        while (count++ < 3) {
-//            int y = (1 + 2 * count) % 3;
-//            switch (y) {
-//                default:
-//                case 0:
-//                    x -= 1;
-//                    break;
-//                case 1:
-//                    x += 5;
-//            }
-//        }
-//        System.out.println(x);
-//
-//        System.out.println("----------");
+        boolean keepGoing = true;
+        int count = 0;
+        int x = 3;
+        while (count++ < 3) {
+            int y = (1 + 2 * count) % 3;
+            switch (y) {
+                default:
+                case 0:
+                    x -= 1;
+                    break;
+                case 1:
+                    x += 5;
+            }
+        }
+        System.out.println(x);
+
+        System.out.println("----------");
         //     	What is the output of the following code snippet?
         System.out.print("a");
         try {
