c4ea9d65f4f482f57d40dc1cb949a6c954ba59fa
wsen <w_sen@flashbox.de>
2018-07-04 09:25:37 +0200

    chore(gfn) AtomicTest

---
+++ Playground/src/de/gfn/ocp/concurrencyw/AtomicTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.ocp.concurrencyw;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author student
+ */
+public class AtomicTest {
+    
+    AtomicInteger count = new AtomicInteger(0);
+    static Integer j = 0;
+    
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for(int i = 0; i< 10000; i++){
+                j = j + 1;
+            }
+        });
+        
+         Thread t2 =new Thread(() -> {
+            for(int i = 0; i< 10000; i++){
+                j = j + 1;
+            }
+        });
+        
+        t1.start();
+        t2.start(); 
+         
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex){
+            System.out.println(ex);
+        }
+    }
+}
