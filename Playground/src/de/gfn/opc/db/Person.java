a8bdb29c7463ce37bf9e8882ad968c04609dd109
Tomasz <tlubowiecki@10.7.10.28>
2018-07-17 12:03:52 +0200

    JDBC

---
+++ Playground/src/de/gfn/opc/db/Person.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.opc.db;
+
+/**
+ *
+ * @author tlubowiecki
+ */
+public class Person {
+    
+    private int id;
+    
+    private String firstname;
+    
+    private String lastname;
+
+    public Person() {
+    }
+    
+    public Person(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "#" + id + ": " + firstname + " " + lastname;
+    }
+    
+    
+}
