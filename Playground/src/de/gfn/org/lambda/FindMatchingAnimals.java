92280fbd0a7ca813becaace539216454de8d4c3d
wsen <w_sen@flashbox.de>
2018-07-17 17:23:18 +0200

    chore(gfn): Lambda DBconnectionTest

---
+++ Playground/src/de/gfn/org/lambda/FindMatchingAnimals.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.org.lambda;
+
+/**
+ *
+ * @author student
+ */
+public class Animal {
+
+    private String species;
+    private boolean canHop;
+    private boolean canSwim;
+
+    public Animal(String speciesName, boolean hopper, boolean swimmer) {
+        species = speciesName;
+        canHop = hopper;
+        canSwim = swimmer;
+    }
+
+    public boolean canHop() {
+        return canHop;
+    }
+
+    public boolean canSwim() {
+        return canSwim;
+    }
+
+    public String toString() {
+        return species;
+    }
+}
+
+//public interface CheckTrait {
+//
+//    public boolean test(Animal a);
+//}
+
+//Now  that we’ve defined a structure, let’s do something with it. The following simple program
+//uses a lambda expression to determine if some sample animals match the specified criteria:
+public class FindMatchingAnimals {
+
+    private static void print(Animal animal, CheckTrait trait) {
+        if (trait.test(animal)) {
+            System.out.println(animal);
+        }
+    }
+
+    public static void main(String[] args) {
+        print(new Animal("fish", false, true), a -> a.canHop());
+        print(new Animal("kangaroo", true, false), a -> a.canHop());
+    }
+
+}
