4ffe5df1e18b85d7827e90ac4b38db59e01e0792
w_sen <w_sen@flashbox.de>
2018-08-13 08:25:20 +0200

    chore(home) OCA Reloaded

---
+++ Playground/src/de/gfn/org/packages/a_employee/WaterFiller.java
@@ -0,0 +1,33 @@
+package de.gfn.org.packages.a_employee;
+
+/**
+ *
+ * @author wsen
+ */
+import de.gfn.org.packages.aquarium.*;
+
+//import de.gfn.org.packages.aquarium.Water;
+//import de.gfn.org.packages.aquarium.jellies.*;
+
+//import de.gfn.org.packages.aquarium.*;
+//import de.gfn.org.packages.aquarium.jellies.Water;
+
+//import de.gfn.org.packages.aquarium.*;
+//import de.gfn.org.packages.aquarium.jellies.*;
+
+//import de.gfn.org.packages.aquarium.Water;
+//import de.gfn.org.packages.aquarium.jellies.Water;
+
+public class WaterFiller {
+    Water water;
+    
+    //public static void main(String[] fargs) { //OK
+    //static public void main(String[] args) {  //OK
+    //public static void main(String args[]) {  //OK
+    //public static void main(String... args) {   //OK
+    //public static void main(String args[]) {  //OK
+     //private static void main(String[] args) { //OK
+     // public static final main(String[] args) {
+        
+    }
+}
---
+++ Playground/src/de/gfn/org/packages/aquarium/Water.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gfn.org.packages.aquarium;
+
+/**
+ *
+ * @author wsen
+ */
+public class Water {
+    
+}
